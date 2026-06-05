b0e704906a53e5ab898973b485b9285aa211fe8f
JustMarvell <marvelmakaluwu050405@gmail.com>
2026-06-05 13:48:48 +0800

    refactor config.py

--- config.py
+++ config.py
@@ -1,11 +1,18 @@
 YDL_OPTS = {
-    'quiet' : True,
-    'skip_download' : True,
-    'default_search' : 'ytsearch5',
-    'runtime' : 'nodejs',
+    'quiet': True,
+    'skip_download': True,
+    'default_search': 'ytsearch5',
+    'noplaylist': False,
+}
+
+YDL_OPTS_PLAY = {
+    **YDL_OPTS,
+    'default_search': 'ytsearch1',
+    'extract_flat': False,
+    'format': 'bestaudio/best',
 }
 
 FFMPEG_OPTIONS = {
-    'before_options' : '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
-    'options' : '-vn',
+    'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
+    'options': '-vn',
 }
